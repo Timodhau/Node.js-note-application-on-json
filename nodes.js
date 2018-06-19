5a11a57371eb92badd37a4eb38e50fab01a63efe
unknown <timodhau@yahoo.fr>
2018-06-20 01:54:10 +0200

    commit

--- nodes.js
+++ nodes.js
@@ -5,19 +5,23 @@ const fs = require('fs');
 var addNote = (title, body) => {
   var notes = [];
   var note = {
-    title,
+    title : title, // {} represent the json
     body
   };
 
-  try{
+  try {
     var notesString = fs.readFileSync('notes-data.json');
     notes = JSON.parse(notesString);
-} catch (e) {
+  } catch (e) {
 
-}
+  }
 
-  notes.push(note);
-  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  var duplicateNotes = notes.filter((note)=> note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 var getAll = () => {
