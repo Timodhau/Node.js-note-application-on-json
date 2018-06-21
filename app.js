a2700ab5c84bc2448867b97910b193640f21f9ab
unknown <timodhau@yahoo.fr>
2018-06-22 00:48:12 +0200

    commit

--- app.js
+++ app.js
@@ -1,25 +1,61 @@
-console.log('starting');
-
-const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
+const fs = require('fs');
 
 const notes = require('./nodes.js')
 
-var argv = yargs.argv;
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't'  // t=  <=> --title=
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b'
+};
+
+const argv = yargs
+  .command('add','Add a new note',{
+    title: titleOptions,
+    body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+    title: titleOptions
+  })
+  .command('remove', 'Remove a note',{
+    title: titleOptions
+  })
+  .help()
+  .argv;
 var command = yargs.argv._[0];
-console.log('command', command);
-console.log('argv', argv);
 
 if (command === 'remove'){
-  notes.removeNote(argv.title);
+  var noteRemoved = notes.removeNote(argv.title);
+  var message = noteRemoved ? 'note was removed' : 'note not found';
+  console.log(message);
 } else if (command === 'add'){
   console.log('adding new note');
-  notes.addNote(argv.title, argv.body);
+  var note = notes.addNote(argv.title, argv.body);
+  if(note){
+    console.log('note created');
+    notes.logNote(note);
+  } else {
+    console.log('note title taken');
+    }
 } else if (command === 'list'){
-  notes.getAll();
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
 } else if (command === 'read'){
-  notes.getNote(argv.title);
+  var note = notes.getNote(argv.title);
+  if (note) {
+    console.log('note created');
+    notes.logNote(note);
+  } else {
+    console.log('note not found')
+  }
 } else{
   console.log('not recognize')
 }
--- nodes.js
+++ nodes.js
@@ -1,44 +1,62 @@
-console.log('starting node.js')
-
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var SaveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+}
+
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title : title, // {} represent the json
     body
   };
 
-  try {
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-  } catch (e) {
-
-  }
-
   var duplicateNotes = notes.filter((note)=> note.title === title);
 
   if (duplicateNotes.length === 0) {
     notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
-  }
+    SaveNotes(notes);
+    return note;
+  };
 };
 
 var getAll = () => {
-  console.log('Getting all notes');
+  return fetchNotes();
 };
 
 var getNote = (title) => {
-  console.log('getting note', title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title === title);
+  return filteredNotes[0];
 };
 
 var removeNote = (title) => {
-  console.log('remove note', title);
+  var notes = fetchNotes();
+  var removingNotes = notes.filter((note)=> note.title !== title);
+  SaveNotes(removingNotes);
+
+  return notes.length !== removingNotes.length;
+};
+
+var logNote = (note) => {
+  console.log('---')
+  console.log(`Title: ${note.title}`);
+  console.log(`Body: ${note.body}`);
 };
 
 module.exports = {
   addNote,
   getAll,
   getNote,
-  removeNote
+  removeNote,
+  logNote
 };
